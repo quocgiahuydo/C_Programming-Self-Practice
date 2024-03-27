4f1b733b9f3fd412fbe68163bc73e2cdd9ec334b
qdo <qdo@binghamton.edu>
2024-03-27 14:03:24 -0400

    addd more

--- foo.c
+++ foo.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+
+void bar(int a, int b){
+    printf("The value of a: %d, and b %d\n", a,b);
+    printf("The value address of a: %p, and address of b %p\n", &a,&b);
+    
+}
 void foo(){
     int q =0;
     static int z =0;
+
+    printf("The value of q: %d, and z %d\n", q,z);
+    printf("The value address of q: %p, and address of z %p\n", &q,&z);
+    printf("\n");
 
     for (int i=0; i<10; i++){
         z++;
@@ -10,12 +20,25 @@ void foo(){
     }
     printf("The value of q: %d, and z %d\n", q,z);
     printf("The value address of q: %p, and address of z %p\n", &q,&z);
-    
 
+    printf("\n");
+    
+    bar(10,8);
 
 }
 
 int main(){
     foo();
+    printf("\n");
+
+    printf("\n");
+    foo();
+    printf("\n");
+
+    printf("\n");
+    foo();
+    printf("\n");
+
+    printf("\n");
     foo();
 }
