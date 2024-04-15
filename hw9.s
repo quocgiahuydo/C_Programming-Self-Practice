7af5e55532c92beb9629bf5ca8904abdec0f3977
qdo <qdo@binghamton.edu>
2024-04-14 20:06:52 -0400

    asembly add

--- hw9.s
+++ hw9.s
@@ -1,5 +1,7 @@
+# Harry Do       
         .data
 arr:   .word 10, 2, 31, 44, 0, 9
+prompt: .asciiz "Result = "
         .text
 main:   la $s0, arr
         li $s1, 0
@@ -13,7 +15,10 @@ FOR:    lw $t1, 0($s0)
         add $s1, $s1, $t1
         j SKIP
 
-DONE:   li $v0, 1
+DONE:   li $v0, 4
+        la $a0, prompt
+        syscall
+        li $v0, 1
         add $a0, $s1, $0
         syscall
         jr $ra
